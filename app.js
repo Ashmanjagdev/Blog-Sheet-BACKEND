fa639b4e4be89bec26b03954ac557ee94ae9f977
Ashman Jagdev <you@example.com>
2022-11-30 17:09:50 +0530

    first commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app=express();
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
 
 const blogSchema=new mongoose.Schema ({
   heading: String,
